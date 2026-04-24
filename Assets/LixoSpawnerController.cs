eea1772e560ae6de4dc31a8ba7a4d1614d8b92b7
Samambaia59 <gunevesgustavo@gmail.com>
2026-04-24 13:09:01 -0300

    agora vai não é possível

---
+++ Assets/LixoSpawnerController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LixoSpawnerController : MonoBehaviour
+{
+    public float maximumX;
+    public float fixedY;
+    public float fixedZ;
+    public float timer;
+    public GameObject Lixo;
+    public int MaxPoints;
+    public int points = 0;
+
+    public TMP_Text pointsText;
+    public TMP_Text victoryText;
+    private int counter = 0;
+
+
+    IEnumerator SpawnRoutine()
+    {
+        while (points < MaxPoints)
+        {
+            Instantiate(Lixo, new Vector3(Random.Range(-maximumX, maximumX + 1), fixedY, fixedZ), Quaternion.identity);
+            yield return new WaitForSeconds(timer);
+        }
+        victoryText.gameObject.SetActive(true);
+    }
+    public void AddToPoints(int x)
+    {
+        points += x;
+        pointsText.text = "Pontuação: " + points.ToString();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject Menu = GameObject.FindWithTag("Menu");
+
+        StartCoroutine(SpawnRoutine());
+        pointsText.text = "Pontuação: 0";
+        victoryText.gameObject.SetActive(false);
+    }
+    private void comecar()
+    {
+        GameObject Menu = GameObject.FindWithTag("Menu");
+        if (counter < 1)
+        {
+            if (Menu.activeInHierarcy == false)
+            {
+
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
